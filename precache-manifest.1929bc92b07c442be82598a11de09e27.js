28876290d625506f0058c783ce75b60afe1a38b6
JaganRaja <jaganraja479@gmail.com>
2020-08-10 17:45:43 +0530

    Updates

---
+++ precache-manifest.1929bc92b07c442be82598a11de09e27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a97ad0a223413160004021468d20e5a",
+    "url": "/React-in-github-demo/index.html"
+  },
+  {
+    "revision": "ef11d0df427075a4b9e0",
+    "url": "/React-in-github-demo/static/css/main.665c052e.chunk.css"
+  },
+  {
+    "revision": "675972f3c16b7597aea8",
+    "url": "/React-in-github-demo/static/js/2.f62897ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-in-github-demo/static/js/2.f62897ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef11d0df427075a4b9e0",
+    "url": "/React-in-github-demo/static/js/main.1cd4e9ed.chunk.js"
+  },
+  {
+    "revision": "05450d120b961b1d4631",
+    "url": "/React-in-github-demo/static/js/runtime-main.214b71a5.js"
+  }
+]);
